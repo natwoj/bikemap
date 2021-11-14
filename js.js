b73b94427ffbd864aa415c980189c22a77e6428f
natwoj <79479413+natwoj@users.noreply.github.com>
2021-11-14 21:51:29 +0100

    baselayer obj + overlay mainTrails obj

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 /* setting start marker/set view */
 var mymap = L.map('map').setView([
-    50.26300965209187, 19.023685455322266], 15);
+    50.26300965209187, 19.023685455322266], 9);
 
 /* set my location */
     mymap.locate({
@@ -8,7 +8,7 @@ var mymap = L.map('map').setView([
     });
 
 /* thunderforest cycle map tile layer */
-var Thunderforest_OpenCycleMap = L.tileLayer('https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=4fbca27dba3f450787b52b350d86ddaf', {
+let Thunderforest_OpenCycleMap = L.tileLayer('https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=4fbca27dba3f450787b52b350d86ddaf', {
 attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 apikey: '<4fbca27dba3f450787b52b350d86ddaf>',
 maxZoom: 22
@@ -30,23 +30,20 @@ alert("You're not sharing your location with us. We can not show you bike trails
 mymap.on('locationerror', onLocationError);
 
 /* creating layer of main trails & adding to map*/
-let ironTrail = L.marker([49.9454207, 18.6101103]).bindPopup('Iron Trail beginning');
-let eaglesNestTrail  = L.marker([50.79646, 19.12409]).bindPopup('Eagles Nests Trail beginning');
-let vistulaTrail  = L.marker([49.6473215, 18.867739]).bindPopup('Vistula Trail beginning')
+let ironTrail = L.marker([49.9454207, 18.6101103]).bindPopup('Iron Trail beginning'),
+    eaglesNestTrail  = L.marker([50.79646, 19.12409]).bindPopup('Eagles Nests Trail beginning'),
+    vistulaTrail  = L.marker([49.6473215, 18.867739]).bindPopup('Vistula Trail beginning');
 
 let mainTrails = L.layerGroup([ironTrail, eaglesNestTrail, vistulaTrail]);
-mainTrails.addTo(mymap);
+let mainTrailsAdded = mainTrails.addTo(mymap);
 
 /*creating controls between layers */
 var baseMap = {
-    "Thunderforest": Thunderforest_OpenCycleMap,
-};
-
+    "Podkład główny": Thunderforest_OpenCycleMap
+}
 var overlayMap = {
-    "Main trails": mainTrails;
-};
+    "Trasy główne": mainTrails
+}; 
 
-L.control.layers(Thunderforest_OpenCycleMap,layerAddedToMap).addTo(mymap);
-
-
+L.control.layers(baseMap, overlayMap).addTo(mymap);
 
