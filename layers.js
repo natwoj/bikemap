43ef6d77482f45a3cfc5f5f1b42d117dbb6c5610
natwoj <79479413+natwoj@users.noreply.github.com>
2021-11-14 17:11:45 +0100

    Add files via upload
    working on main trails layer

--- layers.js
+++ layers.js
@@ -1,8 +1,23 @@
 
+/* creating layer of main trails */
 let mainTrails = L.layerGroup();
 
-let ironTrail = L.marker([39.61, -105.02]).bindPopup('Iron Trail').addTo(mainTrails);
-let eaglesNestTrail  = L.marker([39.74, -104.99]).bindPopup('Eagles Nests Trail').addTo(mainTrails);
+let ironTrail = L.marker([49.9454207, 18.6101103]).bindPopup('Iron Trail').addTo(mainTrails);
+let eaglesNestTrail  = L.marker([50.79646 19.12409]).bindPopup('Eagles Nests Trail').addTo(mainTrails);
+let vistulaTrail  = L.marker([49.6473215, 18.867739]).bindPopup('Vistula Trail').addTo(mainTrails);
+
+
+var map = L.map('map', {
+    center: [49.9454207, 18.6101103],
+    zoom: 10,
+    layers: [mainTrails];
+});
+
+var overlayMaps = {
+    "main trails": mainTrails,
+};
+
+L.control.layers(overlayMaps).addTo(mymap);
 
 
    
