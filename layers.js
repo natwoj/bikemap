259c4e64a0ab415a8514d0cc74d5c2ee93705319
natwoj <79479413+natwoj@users.noreply.github.com>
2021-11-14 16:34:30 +0100

    main trails layer

---
+++ layers.js
@@ -0,0 +1,10 @@
+
+let mainTrails = L.layerGroup();
+
+let ironTrail = L.marker([39.61, -105.02]).bindPopup('Iron Trail').addTo(mainTrails);
+let eaglesNestTrail  = L.marker([39.74, -104.99]).bindPopup('Eagles Nests Trail').addTo(mainTrails);
+
+
+   
+
+
